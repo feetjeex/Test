1ffd85cd86d78f6c9d52e89600e5d9d1400a2b2c
Felix <felixmooij2@gmail.com>
2019-04-09 15:50:01 +0200

    CommitTest

---
+++ testers.py
@@ -0,0 +1,17 @@
+
+
+def main():
+
+    # Prints 'Hello, world'
+    print("hello, worldddd")
+
+    for var in list(range(5)):
+        print (var)
+
+    fruits = ['Banana', 'Apple', 'Orange']
+
+    for fruit in fruits:
+        print('The current fruit is: ', fruit)
+
+if __name__ == "__main__":
+    main()
